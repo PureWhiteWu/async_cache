1abd5cfe2795aba78501806d89f903e635698ace
Pure White <wudi@purewhite.io>
2021-02-18 18:06:41 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,21 @@ where
 
     fetcher: F,
 
-    error_handler: Option<Box<dyn for<'a> Fn(&'a str, anyhow::Error) -> future::BoxFuture<'a, ()>>>,
-    change_handler: Option<Box<dyn for<'a> Fn(&'a str, T, T) -> future::BoxFuture<'a, ()>>>,
-    delete_handler: Option<Box<dyn for<'a> Fn(&'a str, T) -> future::BoxFuture<'a, ()>>>,
+    error_handler: Option<Box<dyn for<'b> Fn(&'b str, anyhow::Error) -> future::BoxFuture<'a, ()>>>,
+    change_handler: Option<Box<dyn for<'b> Fn(&'b str, T, T) -> future::BoxFuture<'a, ()>>>,
+    delete_handler: Option<Box<dyn for<'b> Fn(&'a str, T) -> future::BoxFuture<'a, ()>>>,
 }
 
 async fn test_func(_: &str) -> Result<usize> {
     Ok(1)
 }
 
-impl<T, FUT, F> Options<T, FUT, F>
+impl<'a, T, FUT, F> Options<'a, T, FUT, F>
 where
     T: Send + Clone + PartialEq,
-    FUT: Future<Output = Result<T>> + Send + 'static,
+    FUT: Future<Output = Result<T>> + Send + 'a,
     // fetcher
-    F: Fn(&str) -> FUT,
+    F: Fn(&'a str) -> FUT + 'a,
 {
     // pub fn new(
     //     refresh_interval: Duration,
@@ -43,14 +43,23 @@ where
     // }
 }
 
-fn wrapper<'a, FUT, T>(
-    f: impl Fn(&'a str) -> FUT + 'static,
-) -> Box<dyn Fn(&'a str) -> futures::future::BoxFuture<'a, Result<T>>>
+fn error_wrapper<'a, FUT>(
+    f: impl Fn(&'a str, anyhow::Error) -> FUT + 'static,
+) -> Box<dyn Fn(&'a str, anyhow::Error) -> futures::future::BoxFuture<'a, ()>>
 where
-    FUT: Future<Output = Result<T>> + Send + 'a,
+    FUT: Future<Output = ()> + Send + 'a,
 {
-    Box::new(|s: &str| f(s).boxed())
+    Box::new(move |s: &str, e: anyhow::Error| f(s, e).boxed())
 }
+
+// fn change_wrapper<'a, FUT, T>(
+//     f: impl Fn(&'a str) -> FUT + 'static,
+// ) -> Box<dyn Fn(&'a str) -> futures::future::BoxFuture<'a, Result<T>>>
+//     where
+//         FUT: Future<Output = ()> + 'a,
+// {
+//     Box::new(|s: &str| f(s).boxed())
+// }
 
 // #[derive(Clone)]
 // pub struct AsyncCache<T>
@@ -74,9 +83,12 @@ where
 //     // pub fn new(options: Options<T>) -> AsyncCacheRef<T> {}
 // }
 
+async fn test_error(_: &str, e: anyhow::Error) {
+    println!("{}", e);
+}
 #[cfg(test)]
 mod tests {
-    use crate::{test_func, wrapper, Options};
+    use crate::{error_wrapper, test_error, test_func, Options};
     use anyhow::Result;
     use futures::future::BoxFuture;
     use futures::FutureExt;
@@ -87,8 +99,8 @@ mod tests {
             refresh_interval: Default::default(),
             enable_expire: false,
             expire_duration: Default::default(),
-            fetcher: wrapper(test_func),
-            error_handler: None,
+            fetcher: test_func,
+            error_handler: Some(error_wrapper(test_error)),
             change_handler: None,
             delete_handler: None,
         };
